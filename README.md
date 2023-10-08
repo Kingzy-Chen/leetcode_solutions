f7d8cf9f767169000d3939df66eb1bbda9ce06f9
Kingzy-Chen <953727416@qq.com>
2023-10-09 00:12:57 +0800

    first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# leetcode_solutions
-The Record of My Solutions to LeetCode
---
+++ 代码随想录/01-数组/704-二分查找.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+	左闭右开区间写法: right = n, while (left < right), right = mid;
+	左闭右开区间写法: right = n - 1, while (left <= right), right = mid - 1;
+	建议联想到 nums 只有一个元素的情况
+*/
+
+int search(vector<int>& nums, int target)
+{
+	int n = nums.size();
+	int left = 0;
+	int right = n - 1;
+
+	while (left <= right) {
+		int mid = (left + right) / 2;
+
+		if (nums[mid] > target) {
+			right = mid - 1;
+		}
+		else if (nums[mid] < target) {
+			left = mid + 1;
+		}
+		else return mid;
+	}
+	return -1;
+}
+
+
+int main()
+{
+	vector<int> nums = {-1, 0, 3, 5, 9, 12};
+	int target = 9;
+	int res = search(nums, target);
+	cout << res << endl;
+}

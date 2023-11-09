094a6dd69a30348c61438474a2beacd55f306c12
Kingzy-Chen <953727416@qq.com>
2023-11-09 23:47:20 +0800

    145-二叉树的后序遍历

---
+++ 代码随想录/07-二叉树/94-二叉树的中序遍历 (递归法).cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+// 二叉树
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(): val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right): val(x), left(left), right(right) {}
+};
+
+// 初始化二叉树
+TreeNode* initTree(vector<int>& nums)
+{
+	if (nums.empty()) return nullptr;
+
+	// 保存节点数组, 若 nums[i] == -1, 则保存空指针
+	vector<TreeNode*> nodes;
+
+	for (int i = 0; i < nums.size(); i++) {
+		if (nums[i] == -1) nodes.push_back(nullptr);
+		else nodes.push_back(new TreeNode(nums[i]));
+	}
+
+	// 构造二叉树, 父节点下标 i, 左孩子下标 2*i+1, 右孩子下标 2*i+2
+	for (int i = 0; i < nodes.size(); i++) {
+		if (nodes[i] == nullptr) continue;
+		int leftIdx = 2 * i + 1;
+		int rightIdx = 2 * i + 2;
+
+		if (leftIdx < nodes.size()) {
+			nodes[i]->left = nodes[leftIdx];
+		}
+		if (rightIdx < nodes.size()) {
+			nodes[i]->right = nodes[rightIdx];
+		}
+	}
+	return nodes[0];
+}
+
+
+// 中序遍历 (递归法)
+void traversal(TreeNode* root, vector<int>& ans)
+{
+	if (root == nullptr) return;
+	traversal(root->left, ans);
+	ans.push_back(root->val);
+	traversal(root->right, ans);
+}
+
+
+vector<int> inorderTraversal(TreeNode* root) 
+{
+	vector<int> ans;
+	if (root != nullptr) traversal(root, ans);
+	return ans;
+}
+
+
+int main()
+{
+	vector<int> nums = {1, 2, 3, 4, 5, -1, 7};
+	TreeNode* root = initTree(nums);
+
+	vector<int> ans = inorderTraversal(root);
+	for (auto t: ans) cout << t << " ";
+	cout << endl;
+
+	return 0;
+}

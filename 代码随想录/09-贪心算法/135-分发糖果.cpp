f4568a119afa3095310d75a65c1038c3938a01f3
Kingzy-Chen <953727416@qq.com>
2024-01-18 01:58:22 +0800

    135-分发糖果

--- 代码随想录/09-贪心算法/135-分发糖果.cpp
+++ 代码随想录/09-贪心算法/135-分发糖果.cpp
@@ -11,13 +11,13 @@ int candy(vector<int>& ratings)
 	vector<int> A(n, 1);
 
 	// 从左往右检查每个孩子的左手边
-    for (int i = 0; i < n - 1; i++) {
+	for (int i = 0; i < n - 1; i++) {
 		if (ratings[i] < ratings[i + 1] && A[i] >= A[i + 1]) {
 			A[i + 1] = A[i] + 1;
 		}
-    }
+	}
 
-    // 从右往左检查每个孩子的右手边
+	// 从右往左检查每个孩子的右手边
 	for (int i = n - 1; i > 0; i--) {
 		if (ratings[i] < ratings[i - 1] && A[i] >= A[i - 1]) {
 			A[i - 1] = A[i] + 1;
